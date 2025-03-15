3566acd0574a1f816ea7b06ca69e73b8fb57edb4
BelikanM <nyundumathryme@gmail.com>
2025-03-15 11:02:28 +0000

    Ajout du service worker et mise à jour du manifeste pour PWA

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,34 +1,51 @@
-const cacheName = 'my-pwa-cache-v1';
-const resourcesToCache = [
+const CACHE_NAME = 'drecouvrement-cache-v1';
+const FILES_TO_CACHE = [
     '/',
     '/index.html',
     '/static/app.js',
     '/static/styles.css',
-    '/static/2uLgjGtq4u4Uh7PTbKG30AvJFaQ.svg', // tes images SVG
+    '/static/2uLgjGtq4u4Uh7PTbKG30AvJFaQ.svg',
     '/static/2uLj3r1phyUxfMGyQyicurrdVpk.svg',
-    '/static/icons', // si tu veux aussi mettre en cache ton dossier icons
+    '/static/icons',
     '/static/manifest.json',
     '/static/vectorizer.pkl',
-    '/static/model.pkl',
+    '/static/model.pkl'
 ];
 
+// Installation du service worker et mise en cache des fichiers
 self.addEventListener('install', (event) => {
     event.waitUntil(
-        caches.open(cacheName)
+        caches.open(CACHE_NAME)
             .then((cache) => {
-                return cache.addAll(resourcesToCache);
+                return cache.addAll(FILES_TO_CACHE);
             })
     );
 });
 
+// Activation du service worker
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// Gestion des requêtes en utilisant le cache
 self.addEventListener('fetch', (event) => {
     event.respondWith(
-        caches.match(event.request)
-            .then((cachedResponse) => {
-                if (cachedResponse) {
-                    return cachedResponse;
-                }
-                return fetch(event.request);
-            })
+        caches.match(event.request).then((cachedResponse) => {
+            if (cachedResponse) {
+                return cachedResponse;  // Si le fichier est dans le cache, le renvoyer
+            }
+            return fetch(event.request);  // Sinon, faire une requête réseau
+        })
     );
 });
